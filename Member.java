2c83b33ae767ca905793e4a37d90fb11a29930cc
forhope01 <tinamsn01@gmail.com>
2021-04-15 11:49:40 +0900

    제거

--- Member.java
+++ Member.java
@@ -33,14 +33,4 @@ public void setAge(String age) {
 }
 
 
-private static void deleteGrade() {
-   
-
-      Scanner s = new Scanner(System.in);
-      String id = s.nextLine().trim();
-      
-      System.out.println(id + "을 삭제합니다.");
-      map.remove(id);
-
-   }
 }
