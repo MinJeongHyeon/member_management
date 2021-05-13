2808b677e22eaa525a863a543aebb9fb89f32b30
Min Jeong Hyeon <minidiot@naver.com>
2021-05-13 12:25:54 +0900

    메뉴 구성

--- src/ministop/Main.java
+++ src/ministop/Main.java
@@ -1,10 +1,34 @@
 package ministop;
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		int menu = 0;
+		while(true) {
+			System.out.println("--------------------");
+			System.out.println("1. 상품 입고");
+			System.out.println("2. 상품 목록");
+			System.out.println("3. 상품 삭제");
+			System.out.println("4. 상품 판매");
+			System.out.println("5. 판매 로그");
+			System.out.println("6. 프로그램 종료");
+			System.out.println("--------------------");
+			menu = scanner.nextInt();
+			switch (menu) {
+				case 1:
+					ProductList.warehousing();
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				default:
+					break;
+			}
+		}
 	}
 
 }
--- src/ministop/ProductList.java
+++ src/ministop/ProductList.java
@@ -1,5 +1,11 @@
 package ministop;
+import java.util.*;
 
 public class ProductList {
-
+	ArrayList<Product> productList = new ArrayList<Product>();
+	
+	public static void warehousing() {
+		
+	}
 }
+
