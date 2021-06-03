d3f62ca262393f60d01ec62c15c1fa5d050f5c6a
Dahye000 <jin5032192@gmail.com>
2021-06-03 12:10:42 +0900

    수정(product)

--- src/ShoppingMall/List.java
+++ src/ShoppingMall/List.java
@@ -1,26 +1,39 @@
-package shop;
+package shoppingmall;
 
-public class List {
+static Scanner scanner = new Scanner(System.in);
+static JSONArray array = new JSONArray();
+static JSONParser parser = new JSONParser();
 
-	public static void input() {
-		
-		
-	}
-	
-	public static void output() {
+public static void input() {
+   
+   // JSONObject 생성
+            JSONObject product = new JSONObject();
+         
+            System.out.println("id를 입력해주세요");
+            product.put("id", scanner.nextInt());
+            
+            System.out.println("상품명을 입력해주세요");
+            product.put("productName", scanner.nextInt());
+            
+            System.out.println("가격을 입력해주세요");
+            product.put("price", scanner.next());
+            
+            System.out.println("수량을 입력해주세요");
+            product.put("amount", scanner.next());
+            
+         
+            // JSONArray에 JSONObject 입력
+            array.add(product);
 
-		
-	}
-	
-	public static void delete() {
-		
-	}
-	
-	public static void edit() {
-		
-	}
-	
-	public static void search() {
-		
-	}
+            // 파일에 덮어쓰기
+            try {
+               FileWriter file = new FileWriter(" product_list.json", false);
+               file.write(array.toJSONString());
+               file.flush();
+               file.close();
+            }
+            catch (IOException e) {
+               e.printStackTrace();
+            }
+   
 }
