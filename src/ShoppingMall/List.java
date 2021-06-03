a5ddf47d7879168fe7e4c510dd281412d46a673a
Dahye000 <jin5032192@gmail.com>
2021-06-03 12:34:42 +0900

    입력 메소드(주석 수정)

--- src/ShoppingMall/List.java
+++ src/ShoppingMall/List.java
@@ -1,54 +1,17 @@
-<<<<<<< HEAD
-/**
-   @name output
-   @title 상품 출력
-   @detail 상품을 출력하는 메소드
-   @author 김영임
-   @since 2021.06.03
-   @version 1.0
-   ============edit log============
-   Date - Author - Note
-   2021.06.03 - 김영임 - 초안 작성
 
- */
-
-public static void output() {
-      try {
-         Object object = parser.parse(new FileReader("product_list.json"));
-         array = (JSONArray) object;
-         for (int i=0; i<array.size(); i++) {
-            JSONObject obj = (JSONObject) array.get(i);
-            System.out.print("아이디: " + obj.get("id") + "\t");
-            System.out.print("이름: " + obj.get("productName") + "\t");
-            System.out.print("가격: " + obj.get("price") + "\t");
-            System.out.println("수량: " + obj.get("amount") + "\t");
-         }
-         
-      }
-      catch (FileNotFoundException e) { e.printStackTrace(); } 
-      catch (IOException e) { e.printStackTrace(); } 
-      catch (ParseException e) { e.printStackTrace(); }
-
-      
-   }
-
-/**
-   @name delete
-   @title 상품 삭제 메소드
-   @detail 상품을 삭제하는 메소드
-   @author 김동엽
-   @since 2021.06.03
-   @version 1.0
-   ============edit log============
-   Date - Author - Note
-   2021.06.03 - 김동엽 - 초안 작성
-=======
 package shoppingmall;
->>>>>>> 51a16106103ece8b6d91423383fae68f19d6f80c
 
 static Scanner scanner = new Scanner(System.in);
 static JSONArray array = new JSONArray();
 static JSONParser parser = new JSONParser();
+
+/**
+@name Input
+@title 상품 입력 메소드
+@detail 메소드로 JSON파일에 상품(ID,상품명,가격,수량) 입력
+@author 진다혜
+
+*/
 
 public static void input() {
    
