8b56dc3b2e1b38ff18e1c61923a3b7eeba591919
tammy0623 <moon_bh_@naver.com>
2021-06-03 12:29:24 +0900

    상품 출력

--- src/ShoppingMall/List.java
+++ src/ShoppingMall/List.java
@@ -1,4 +1,37 @@
 /**
+   @name output
+   @title 상품 출력
+   @detail 상품을 출력하는 메소드
+   @author 김영임
+   @since 2021.06.03
+   @version 1.0
+   ============edit log============
+   Date - Author - Note
+   2021.06.03 - 김영임 - 초안 작성
+
+ */
+
+public static void output() {
+      try {
+         Object object = parser.parse(new FileReader("product_list.json"));
+         array = (JSONArray) object;
+         for (int i=0; i<array.size(); i++) {
+            JSONObject obj = (JSONObject) array.get(i);
+            System.out.print("아이디: " + obj.get("id") + "\t");
+            System.out.print("이름: " + obj.get("productName") + "\t");
+            System.out.print("가격: " + obj.get("price") + "\t");
+            System.out.println("수량: " + obj.get("amount") + "\t");
+         }
+         
+      }
+      catch (FileNotFoundException e) { e.printStackTrace(); } 
+      catch (IOException e) { e.printStackTrace(); } 
+      catch (ParseException e) { e.printStackTrace(); }
+
+      
+   }
+
+/**
    @name delete
    @title 상품 삭제 메소드
    @detail 상품을 삭제하는 메소드
